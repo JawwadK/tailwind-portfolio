cdf4b2b8372391990ec3acaaa1c35e10844f2b9b
Jawwad Khan <jawwad1809@gmail.com>
2023-06-15 10:33:27 -0400

    Removed unused linked component import statment

--- components/Content.tsx
+++ components/Content.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-import { Link } from "react-scroll/modules";
 import { FaLinkedin, FaGithub } from "react-icons/fa";
 import { MdEmail } from "react-icons/md"
 
