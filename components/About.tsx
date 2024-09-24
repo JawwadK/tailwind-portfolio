27de6ee11b7d9417a9e07b7f75999d7378bfb8ec
Jawwad Khan <jawwad1809@gmail.com>
2024-09-24 02:10:51 -0400

    fixed issues with mobile screens

--- components/About.tsx
+++ components/About.tsx
@@ -1,30 +1,32 @@
 import React from "react";
 import SlideUp from "./SlideUp";
 
-
-
 const About = () => {
   return (
-    <section id="about">
+    <section id="about" className="mt-16">
+      {" "}
+      {/* Add margin-top to create space between sections */}
       <div className="my-4 pb-12 md:pt-8 md:pb-12">
         <h1 className="text-center font-bold text-4xl">
           About Me
           <hr className="w-6 h-1 mx-auto my-4 bg-cyan-900 border-0 rounded-full" />
         </h1>
         <SlideUp offset="-300px 0px -300px 0px">
-          <div className="flex flex-col space-y-10 items-stretch justify-center align-top  md:text-left md:p-4 md:space-y-0 md:space-x-10">
+          <div className="flex flex-col space-y-10 items-stretch justify-center align-top md:text-left md:p-4 md:space-y-0 md:space-x-10">
             <div className="">
               <p className="text-xl">
-                I enjoy building, designing and developing websites and
+                I enjoy building, designing, and developing websites and
                 applications. I am passionate about software development,
-                process automation and working with data to create meaningful
+                process automation, and working with data to create meaningful
                 insights to help data-driven decision making. I am a fast
                 learner and hold my code to a high standard to ensure quality
                 results are delivered on time.
               </p>
             </div>
           </div>
-          <div className="">
+          <div className="mt-10">
+            {" "}
+            {/* Add margin-top for separation */}
             <h1 className="text-center text-4xl font-bold">My Skills</h1>
             <hr className="w-6 h-1 mx-auto my-4 bg-cyan-900 border-0 rounded-full" />
           </div>
--- components/Content.tsx
+++ components/Content.tsx
@@ -15,11 +15,11 @@ const Content = () => {
     <>
       <section
         id="home"
-        className="h-screen flex flex-col justify-center items-center"
+        className="min-h-screen flex flex-col justify-center items-center overflow-auto"
       >
         {/* Headshot Section */}
         <SlideInAnimation direction="left">
-          <div className="w-full h-auto flex justify-center items-center mt-64 sm:mt-64">
+          <div className="w-full h-auto flex justify-center items-center sm:mt-64">
             <Image
               className="rounded-full shadow-3xl"
               src="/Headshot Cropped.jpg"
@@ -30,7 +30,7 @@ const Content = () => {
           </div>
         </SlideInAnimation>
         {/* Text Section */}
-        <div className="flex flex-col text-center items-center justify-center mt-12 pt-16 pb-32 sm:py-32 md:text-left md:w-5/5">
+        <div className="flex flex-col text-center items-center justify-center mt-12 md:pb-16 sm:py-32 md:text-left md:w-5/5">
           <TypingAnimation />
           <SlideInAnimation direction="left">
             <p className="text-lg mt-4 mb-6 md:text-2xl text-center md:text-left">
@@ -39,44 +39,47 @@ const Content = () => {
             </p>
 
             <p className="text-lg mt-4 mb-6 md:text-2xl text-center md:text-left">
-              Lets talk about Data, AI and Cloud Architecture
+              Let's talk about Data, AI, and Cloud Architecture
             </p>
           </SlideInAnimation>
+
+          {/* Social Buttons Section */}
           <SlideInAnimation direction="right">
-            <div className="pt-6 md:px-4 text-center md:text-left">
+            <div className="pt-6 md:px-4 flex space-x-6 justify-center md:justify-start">
               <button
-                className="px-6 py-3 rounded transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-100"
+                className="px-4 py-2 rounded transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-100"
                 onClick={() => handleButtonClick("https://github.com/JawwadK")}
               >
                 <FaGithub
                   className="text-cyan-950 hover:text-cyan-500 dark:text-white"
-                  size={90}
+                  size={70}
                 />
               </button>
               <button
-                className="px-6 py-3 rounded transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-100"
+                className="px-4 py-2 rounded transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-100"
                 onClick={() =>
                   handleButtonClick("https://www.linkedin.com/in/jawwad77/")
                 }
               >
                 <FaLinkedin
                   className="text-cyan-950 hover:text-cyan-500 dark:text-white"
-                  size={90}
+                  size={70}
                 />
               </button>
             </div>
           </SlideInAnimation>
-        </div>
 
-        <div className="flex flex-row text-center items-center justify-center mb-12 py-8">
+          {/* Resume Button */}
           <SlideInAnimation direction="right">
-            <button
-              className="flex items-center justify-center text-3xl text-neutral-100 font-semibold px-6 py-3 bg-cyan-950 rounded shadow hover:bg-cyan-600 dark:bg-white dark: dark:text-neutral-950 transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-100"
-              onClick={() => handleButtonClick("/Jawwad_Resume.pdf")}
-            >
-              <FaFilePdf size={45} className="mr-2" />
-              <span>Resumé</span>
-            </button>
+            <div className="mt-8 flex justify-center">
+              <button
+                className="flex items-center justify-center text-2xl sm:text-3xl font-semibold px-6 py-3 bg-cyan-950 text-neutral-100 rounded shadow hover:bg-cyan-600 dark:bg-white dark:text-neutral-950 transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-100"
+                onClick={() => handleButtonClick("/Jawwad_Resume.pdf")}
+              >
+                <FaFilePdf size={35} className="mr-3" />
+                <span>Resumé</span>
+              </button>
+            </div>
           </SlideInAnimation>
         </div>
       </section>
